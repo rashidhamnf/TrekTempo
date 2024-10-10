c3884b441d47768366d7c35d044bbe29cddbcdc9
Rashidha <139774901+rashidhamnf@users.noreply.github.com>
2024-10-10 12:14:42 +0530

    Update EventPage.dart
    change event page bookmark buttons

--- Front- End/lib/Pages/HomePage_Featurs/Event/EventPage.dart
+++ Front- End/lib/Pages/HomePage_Featurs/Event/EventPage.dart
@@ -12,230 +12,196 @@ class EventPage extends StatefulWidget {
 }
 
 class _HomeState extends State<EventPage> {
-  bool csfcover = false, laksapana = false, motor = false;
+ List<Event> events = [
+    Event(
+      image: "images/csf_cover.png",
+      title: "COLOMBO SHOPPING FESTIVAL - CSF",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+    Event(
+      image: "images/laksapana.jpeg",
+      title: "Lakshapana waterfall abseiling",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+    Event(
+      image: "images/motor.jpeg",
+      title: "Colombo Motor Show 2024",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+    Event(
+      image: "images/csf_cover.png",
+      title: "COLOMBO SHOPPING FESTIVAL - CSF",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+    Event(
+      image: "images/laksapana.jpeg",
+      title: "Lakshapana waterfall abseiling",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+    Event(
+      image: "images/motor.jpeg",
+      title: "Colombo Motor Show 2024",
+      dateTime: "Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m | at BMICH",
+      isBookmarked: false,
+    ),
+  ];
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Events'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => MainHomePage()),
-            );
-          },  
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => AddEventPage.AddEventPage(),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 198, 177, 177),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Icon(Icons.arrow_back, color: Colors.black),
+                  ),
+                  Text(
+                    "Events",
+                    style: AppWidget.boldTextFieldStyle(),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 198, 177, 177),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddEventPage(),
+                          ),
+                        );
+                      },
+                      child: const Icon(Icons.add, color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 18.0),
+              showItem(),
+            ],
           ),
-              );
-            },
-          ),
-        ],
-
-      ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10.0),
-            Text(
-              "Upcoming Events",
-              style: AppWidget.LightTextFeildStyle(),
-            ),
-            const SizedBox(height: 20.0),
-            showItem(),
-          ],
         ),
       ),
     );
   }
 
   Widget showItem() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Details()),
-            );
-            csfcover = true;
-            laksapana = false;
-            motor = false;
-            setState(() {});
-          },
-         child: Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                 child: Container(
-                     decoration: BoxDecoration(color: csfcover? Color.fromARGB(255, 95, 198, 221): Colors.white,borderRadius: BorderRadius.circular(10)),
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: events.length, // Number of events
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            eventCard(
+              events[index].image,
+              events[index].title,
+              events[index].dateTime,
+              events[index].isBookmarked,
+              () {
+                // Handle event tap (navigate to details or perform other actions)
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Details()));
+              },
+              () {
+                // Toggle bookmark state for the clicked event
+                setState(() {
+                  events[index].isBookmarked = !events[index].isBookmarked;
+                });
+              },
+            ),
+            const SizedBox(height: 30.0),
+          ],
+        );
+      },
+    );
+  }
 
-
-                  padding: const EdgeInsets.all(8),
-                 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:[ 
-                    Image.asset("images/CSF-Cover.png", height: 100, width: 300, fit: BoxFit.cover),
-                  
-                   
-
-                Text("COLOMBO SHOPPING FESTIVAL - CSF", style: AppWidget.semiBooldTextFieldStyle()),
-                
-                  Row(
-  children: [ 
-                  
-                 Text("Dec - 2024 , 05 06 & 07\n 10:00 a.m - 10:00 p.m | at BMICH ", style: AppWidget.LightTextFeildStyle()),
+  Widget eventCard(
+    String image,
+    String title,
+    String dateTime,
+    bool isBookmarked,
+    VoidCallback onTap,
+    VoidCallback onBookmarkTap,
+  ) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(image, height: 100, width: 300, fit: BoxFit.cover),
+              Text(title, style: AppWidget.semiBooldTextFieldStyle()),
+              Row(
+                children: [
+                  Text(dateTime, style: AppWidget.LightTextFeildStyle()),
                   Spacer(),
-
-
-
-                   Container(
-                padding: const EdgeInsets.all(3),
-              
-              decoration: BoxDecoration(color:Color.fromARGB(255, 126, 128, 125),borderRadius: BorderRadius.circular(8) ),
-              child: const Icon(Icons.bookmark, color:Color.fromARGB(255, 255, 255, 255),
-              ),
-              
-              ),
-                  
-  ],
-)
-                  ],
+                  GestureDetector(
+                    onTap: onBookmarkTap,
+                    child: Container(
+                      padding: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        color: isBookmarked
+                            ? Color.fromARGB(255, 43, 253, 155)
+                            : Color.fromARGB(255, 253, 254, 253),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(
+                        isBookmarked ? Icons.bookmark : Icons.bookmark_outline,
+                        color: Color.fromARGB(255, 227, 146, 34),
+                      ),
+                    ),
                   ),
-
-                           
-
-                 )
-                ),
-               ),
-               SizedBox(height: 20.0,),
-                     
-
-                    GestureDetector(
-              onTap: () {
-                  csfcover=false;
-                  laksapana=true;
-                  motor=false;
-                  setState(() {
-                    
-                  });
-
-              },
-                child: Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                 child: Container(
-                     decoration: BoxDecoration(color: laksapana?Color.fromARGB(255, 95, 198, 221): Colors.white,borderRadius: BorderRadius.circular(10)),
-
-
-                  padding: const EdgeInsets.all(8),
-                 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  
-                  children:[ Image.asset("images/laksapana.jpeg", height: 100, width: 300, fit: BoxFit.cover),
-                        SizedBox(width: 20),
-
-                       
-                  Text("Lakshapana waterfall abseiling", style: AppWidget.semiBooldTextFieldStyle()),
- Row(
-  children: [
-                  Text("Dec - 2024 , 05 06 & 07\n 10:00 a.m - 10:00 p.m | at BMICH ", style: AppWidget.LightTextFeildStyle()),
-                   Spacer(),
-                   
-                   
-                    Container(
-                padding: const EdgeInsets.all(3),
-              
-              decoration: BoxDecoration(color:Color.fromARGB(255, 126, 128, 125),borderRadius: BorderRadius.circular(8) ),
-              child: const Icon(Icons.bookmark, color:Color.fromARGB(255, 255, 254, 254),
+                ],
               ),
-              
-              )
-                 ],
-)    
-                  ],),
-
-
-                           
-
-                 ),
-                ),
-               ),
-
-                   SizedBox(height: 20.0,),
-                 GestureDetector(
-              onTap: () {
-                  csfcover=false;
-                  laksapana=false;
-                  motor=true;
-                  setState(() {
-                    
-                  });
-
-              },
-                child: Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                 child: Container(
-                     decoration: BoxDecoration(color: motor? Color.fromARGB(255, 95, 198, 221): Colors.white,borderRadius: BorderRadius.circular(10)),
-
-
-                  padding: const EdgeInsets.all(8),
-                 
-                  child:Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  
-                  children:[ Image.asset("images/motor.jpeg", height: 100, width: 300, fit: BoxFit.cover),
-                     
-                  Text("Colombo Motor Show 2024", style: AppWidget.semiBooldTextFieldStyle()),
- Row(
-  children: [ 
-                  Text("Dec - 2024 , 05 06 & 07\n10:00 a.m - 10:00 p.m  | at BMICH ", style: AppWidget.LightTextFeildStyle()),
-                  
-                  Spacer(),
-                   Container(
-                padding: const EdgeInsets.all(3),
-              
-              decoration: BoxDecoration(color:Color.fromARGB(255, 126, 128, 125),borderRadius: BorderRadius.circular(8) ),
-              child: const Icon(Icons.bookmark, color:Color.fromARGB(255, 255, 255, 255),
-              ),
-              
-              )
- ],
-)
-                  ],),
-
- 
- 
-
-
-                           
-
-                 ),
-                ),
-               ),
-
-           ],
-           
-           );
-
-   }
-
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+// Event model to store event data
+class Event {
+  final String image;
+  final String title;
+  final String dateTime;
+  bool isBookmarked;
+
+  Event({
+    required this.image,
+    required this.title,
+    required this.dateTime,
+    this.isBookmarked = false,
+  });
+}
+
